63c84ebaf62aca58a8e54176c43765fb47416484
Mohamed Hamza <mhamza@fastmail.com>
2026-01-13 10:00:52 -0500

    schemadiff: export GetViewDependentTableNames
    Export this function so it can be used by the workflow package to
    validate that views being moved reference tables that are also being
    moved or already exist on the target.
    
    Signed-off-by: Mohamed Hamza <mhamza@fastmail.com>

--- go/vt/schemadiff/schema_test.go
+++ go/vt/schemadiff/schema_test.go
@@ -307,7 +307,7 @@ func TestGetViewDependentTableNames(t *testing.T) {
 			createView, ok := stmt.(*sqlparser.CreateView)
 			require.True(t, ok)
 
-			tables, ctes := getViewDependentTableNames(createView)
+			tables, ctes := GetViewDependentTableNames(createView)
 			assert.Equal(t, ts.tables, tables)
 			assert.Equal(t, ts.ctes, ctes)
 		})
@@ -457,7 +457,7 @@ func TestInvalidSchema(t *testing.T) {
 			//      t10
 			//       ^
 			//       |
-			//t12<->t11<-t13
+			// t12<->t11<-t13
 			schema: `
 				create table t10(id int primary key);
 				create table t11 (id int primary key, i int, i10 int, constraint f111205 foreign key (i) references t12 (id) on delete restrict, constraint f111005 foreign key (i10) references t10 (id) on delete restrict);
@@ -730,7 +730,7 @@ func TestInvalidTableForeignKeyReference(t *testing.T) {
 }
 
 func TestGetEntityColumnNames(t *testing.T) {
-	var queries = []string{
+	queries := []string{
 		"create table t1(id int, state int, some char(5))",
 		"create table t2(id int primary key, c char(5))",
 		"create view v1 as select id as id from t1",
